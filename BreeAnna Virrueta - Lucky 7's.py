065d2b197dfaf40eb1b582323f7089022597a158
BreeAnnaV <33762524+BreeAnnaV@users.noreply.github.com>
2018-01-17 10:44:22 -0800

    1.17.18
    Finished Code

--- BreeAnna Virrueta - Lucky 7's.py
+++ BreeAnna Virrueta - Lucky 7's.py
@@ -5,16 +5,29 @@ import random
 # 3) Add the numbers together to get your total
 # 4) Print the sum on the line below
 
-dice1 = (random.randint(1, 6))
-dice2 = (random.randint(1, 6))
-print(dice1); print(dice2)
-
-total = dice1 + dice2
-print(total)
-
 money = 15
-while total != 7:
-    money - 1
-else:
+rounds = 0
+
+while money > 0:
+    money -= 1
+    dice1 = (random.randint(1, 6))
+    dice2 = (random.randint(1, 6))
+    total = dice1 + dice2
+    print("You rolled a %s" % dice1)
+    print("You also rolled a %s" %dice2)
+    print("Your total is %s" % total)
+    print("Your current amount of money is %s" % money)
     if total == 7:
-        
+        rounds += 1
+        money += 5
+        print("You win")
+        print("Your current total is now %s" % money)
+    if total > 7:
+        rounds += 1
+        print("You lose")
+    if total < 7:
+        rounds += 1
+        print("You lose")
+    if money == 0:
+        print("Game is over, you cannot continue playing")
+        print("You ended the game with %s rounds" % rounds)
